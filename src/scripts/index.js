a5bdf1a3d39efa5e1a65ec4f680c334c17770689
Jabbar <multimediary@gmail.com>
2021-05-20 12:20:19 +0700

    Update Eslin

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,19 +2,19 @@ import 'regenerator-runtime';
 import '../styles/main.css';
 // Fetch data json
 import('../DATA.json').then(({
-  default: jsonData
+  default: jsonData,
 }) => {
-  console.log(jsonData)
-  let datas = jsonData['restaurants']
+  console.log(jsonData);
+  const datas = jsonData.restaurants;
   let dataList = '';
-  datas.forEach(function(data) {
+  datas.forEach((data) => {
     dataList += `
         <div class="list_item">
-            <img class="list_thumbnail" src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}">
-            <div class="ratings">Rating : ${data['rating']} / 5</div>
+            <img class="list_thumbnail" src="${data.pictureId}" alt="${data.name}" title="${data.name}">
+            <div class="ratings">Rating : ${data.rating} / 5</div>
             <div class="list_content">
-                <h1 class="list_item_title"><a href="#">${data['name']} - ${data['city']}</a></h1>
-                <div class="list_item_desc">Description : ${data['description'].slice(0, 111)}...</div>
+                <h1 class="list_item_title"><a href="#">${data.name} - ${data.city}</a></h1>
+                <div class="list_item_desc">Description : ${data.description.slice(0, 111)}...</div>
             </div>
         </div>
         `;
@@ -28,15 +28,15 @@ const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
 
-menu.addEventListener('click', function(event) {
+menu.addEventListener('click', (event) => {
   drawer.classList.toggle('open');
   event.stopPropagation();
 });
 
-hero.addEventListener('click', function() {
+hero.addEventListener('click', () => {
   drawer.classList.remove('open');
 });
 
-main.addEventListener('click', function() {
+main.addEventListener('click', () => {
   drawer.classList.remove('open');
 });
